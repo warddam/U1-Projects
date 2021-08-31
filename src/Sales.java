bc57a2b0e26d5d65508cea359a8f0158503ad159
DJ Ward <warddam001@warren.k12.in.us>
2021-08-31 18:00:19 -0400

    Project 1: Sales

--- src/Sales.java
+++ src/Sales.java
@@ -1,4 +1,7 @@
-public class Sales {
+
+
+
+
 
     /*Write a program, using dialog boxes, which prompts the user to enter a quantity, price, discount(as an integer) and sales tax(as an integer).
     The program should output the subtotal, tax total and total cost. Output should have proper currency format and proper rounding.
@@ -16,4 +19,44 @@ public class Sales {
     Tax Total: $1.57
     Total Cost: $24.03
      */
+
+
+    import javax.swing.JOptionPane;
+
+    public class Sales {
+
+    public static void main(String [] args ) {
+
+      String input;
+      int quantity;
+      int price;
+      int discount;
+      int salestax;
+
+      double subtotal;
+      double totalcost;
+      double taxtotal;
+
+      input = JOptionPane.showInputDialog("Enter Quantity");
+      quantity = Integer.parseInt(input);
+
+        input = JOptionPane.showInputDialog("Enter Price");
+        price = Integer.parseInt(input);
+
+        input = JOptionPane.showInputDialog("Enter Discount");
+        discount = Integer.parseInt(input);
+
+        input = JOptionPane.showInputDialog("Enter Sales Tax");
+        salestax = Integer.parseInt(input);
+
+        subtotal=(price * quantity) - discount;
+        taxtotal=Math.round(subtotal * salestax)/10;
+        totalcost= subtotal + taxtotal;
+
+        JOptionPane.showMessageDialog(null, "Total Cost:$" + subtotal + "\n" + "Tax Total:$" + taxtotal + "\n" + "Total Cost:$" + totalcost);
+
+
+
+
+    }
 }
